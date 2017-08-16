b6218b8201790a489bbda115e3ac589a03106d62
tongjinle <1328192478@qq.com>
2017-08-16 23:36:46 +0800

    init

---
+++ struct/enums.ts
@@ -0,0 +1,28 @@
+// 聊天类型
+// 世界聊天,房间聊天,私人聊天
+export enum EChatType {
+	World,
+	Room,
+	Personal
+};
+
+// 在线, 离线
+export enum EPlatformStatus {
+	Online,
+	Offline
+};
+
+// 空闲,观战,游戏中
+export enum EGameStatus {
+	Free,
+	Watch,
+	Play
+};
+
+
+// 队伍颜色
+export enum EFlagColor {
+	Black,
+	Red,
+	Green
+}
---
+++ struct/protocol.ts
@@ -0,0 +1,126 @@
+import {
+    EChatType,
+    EPlatformStatus,
+    EGameStatus,
+    EFlagColor
+} from './enums';
+
+// ##### to client event #####
+
+// 登录
+export interface IReqLoginData {
+    userName: string;
+    password: string;
+};
+
+export interface IResLoginData {
+    flag: boolean;
+    userName: string;
+};
+
+export interface INotifyLoginData {
+    userName: string;
+};
+
+// 登出
+export interface IReqLogoutData {
+};
+
+export interface IResLogoutData {
+    flag: boolean;
+};
+
+export interface INotifyLogoutData {
+    userName: string;
+};
+
+// 进入房间
+export interface IReqUserJoinRoomData {
+    roomName: string;
+};
+
+export interface IResUserJoinRoomData {
+    flag: boolean;
+    roomName: string;
+};
+
+export interface INotifyJoinRoomData {
+    roomName: string;
+    userName: string;
+};
+
+// 退出房间
+export interface IReqUserLeaveRoomData {
+};
+
+export interface IResUserLeaveRoomData {
+    flag: boolean;
+};
+
+export interface INotifyLeaveRoomData {
+    roomName: string;
+    userName: string;
+};
+
+
+// 聊天
+export interface IReqChat {
+    message: string;
+    type: EChatType
+    // to是对某人的私人聊天
+    to?: string;
+};
+
+export interface IResChat {
+    flag: boolean;
+};
+
+export interface INotifyChat {
+    from: string;
+    type: EChatType;
+    to?: string;
+    message: string;
+    timestamp: number;
+};
+
+// 用户当前的游戏信息
+export interface IGameInfo {
+    // 玩家数量
+    palyerCount: number;
+    // 观战者数量
+    watcherCount: number;
+    // 
+    stepCount: number;
+    startTimestamp: number;
+    endTimestamp: number;
+    currTimestamp: number;
+    winColor: EFlagColor;
+    status: EGameStatus;
+};
+
+// 用户信息
+export interface IUserInfo {
+    userName: string;
+    platformStatus: EPlatformStatus;
+    gameStatus: EGameStatus;
+    roomId: number;
+    gameInfo: IGameInfo;
+};
+
+// 获取在线用户列表
+export interface IReqOnlineUserList {
+
+};
+
+export interface IResOnlineUserList {
+    flag: boolean;
+    list: IUserInfo[];
+};
+
+// 用户列表
+export interface IResUserInfo {
+    userName: string
+};
+
+export interface IResUserList extends Array<IResUserInfo> {
+};
