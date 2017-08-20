ffb0c96a20472804e2f96f5aa623341d12a8ae31
tongjinle <1328192478@qq.com>
2017-08-20 17:02:37 +0800

    登陆测试完成

--- client/source/test.ts
+++ client/source/test.ts
@@ -2,111 +2,137 @@ import * as io from "socket.io-client";
 import * as _ from 'underscore';
 import * as async from 'async';
 
-let port = 1216;
+let watcher;
+let watchedList: { userName: string, status: boolean }[] = [];
+let soList: { [userName: string]: SocketIOClient.Socket } = {};
 
-let opts: SocketIOClient.ConnectOpts = {
-	reconnection: false,
-	// autoConnect:false,
-	forceNew: true
+let createSocket = () => {
+	let port = 1216;
+
+	let opts: SocketIOClient.ConnectOpts = {
+		reconnection: false,
+		// autoConnect:false,
+		forceNew: true
+	};
+	let so: SocketIOClient.Socket = io("http://localhost:1216", opts);
+
+	return so;
 };
 
 
-let userNameList = ['watcher', 'a', 'b', 'c'];
-let soList: { [userName: string]: SocketIOClient.Socket } = {};
-userNameList.forEach(usName => {
-	let so: SocketIOClient.Socket = io("http://localhost:1216", opts);
-	soList[usName] = so;
 
-	so.on('resLogin', data => {
-		let { flag } = data;
-		console.log(`resLogin: ${usName} -- ${flag}`);
-	});
+let createWatcher = () => {
+	watcher = createSocket();
+	watcher.emit('reqLogin', { userName: 'watcher', password: 'falcon' });
+	watcher.on('notiLogin', data => {
+		let { userName } = data;
+		let us = _.find(watchedList, us => us.userName == userName);
+		if (us) {
+			us.status = true;
+		} else {
+			watchedList.push({ userName, status: true });
 
-
-	so.on('resLogout', data => {
-		let { flag } = data;
-		console.log(`resLogout: ${usName} -- ${flag}`);
-	});
-
-	so.on('disconnect', data => {
-		console.log(`disconnect: ${usName}`);
-	});
-
-});
-
-let watcher = soList['watcher'];
-let watchedList: { userName: string, status: boolean }[] = [];
-watcher.on('notiLogin', data => {
-	let { userName } = data;
-	let us = _.find(watchedList, us => us.userName == userName);
-	if (us) {
-		us.status = true;
-	} else {
-		watchedList.push({ userName, status: true });
-
-	}
-});
-
-watcher.on('notiLogout', data => {
-	let { userName } = data;
-	watchedList = watchedList.filter(n => n.userName != userName);
-});
-
-watcher.on('notiDisconnect', data => {
-	let { userName } = data;
-	watchedList.forEach(n => {
-		if (n.userName == userName) {
-			n.status = false;
 		}
 	});
-});
 
-watcher.on('resOnlineUserList', data => {
-	console.log(JSON.stringify(data, null, 4));
-});
+	watcher.on('notiLogout', data => {
+		let { userName } = data;
+		watchedList = watchedList.filter(n => n.userName != userName);
+	});
 
-
-
-async.series([
-	cb => {
-		watcher.emit('reqLogin', { userName: 'watcher', password: 'falcon' });
-		setTimeout(cb, 100);
-	},
-	cb => {
-		userNameList.slice(1).forEach(n => {
-			soList[n].emit('reqLogin', { userName: n, password: 'falcon' });
+	watcher.on('notiDisconnect', data => {
+		let { userName } = data;
+		watchedList.forEach(n => {
+			if (n.userName == userName) {
+				n.status = false;
+			}
 		});
-		setTimeout(cb, 200);
-	},
-	cb => {
-		soList['b'].disconnect();
-		soList['c'].emit('reqLogout');
-		setTimeout(cb, 200);
-	},
-	cb => {
-		console.log(JSON.stringify(watchedList,null,4));
-		cb();
-	},
-	cb => {
-		let usName = 'b';
-		soList[usName] = io("http://localhost:1216", opts);
-		soList[usName].emit('reqLogin', { userName: usName, password: 'falcon' });
-		setTimeout(cb, 2000);
+	});
 
-	},
-	cb => {
-		console.log(JSON.stringify(watchedList,null,4));
-		cb();
-	},
-	cb=>{
-		watcher.emit('reqOnlineUserList');
-		cb();
-	}
-]);
+	watcher.on('resOnlineUserList', data => {
+		console.log(JSON.stringify(data, null, 4));
+	});
+};
 
 
 
+let login = (socket: SocketIOClient.Socket, userName: string) => {
+	socket.emit('reqLogin', { userName, password: 'falcon' });
+};
 
+let logout = (socket: SocketIOClient.Socket) => {
+	socket.emit('reqLogout');
+};
+
+let clear = (cb) => {
+	watchedList = [];
+	_.each(soList, so => {
+		logout(so);
+	});
+
+	setTimeout(cb, 5000);
+};
+
+let testList = [];
+testList.push((cb) => {
+	let userNameList = ['a', 'b', 'c'];
+
+	async.series([
+		cb => {
+			userNameList.forEach(usName => {
+				let so = createSocket();
+				soList[usName] = so;
+				login(so, usName);
+			});
+			setTimeout(cb, 2000);
+		},
+		cb => {
+			soList['b'].disconnect();
+			soList['c'].emit('reqLogout');
+			setTimeout(cb, 2000);
+		},
+		cb => {
+			// c登出就表示不在
+			let c = _.find(watchedList, n => n.userName == 'c');
+			console.assert(c === undefined);
+
+			// b断线,那么其userName还在服务器内存中
+			let b = _.find(watchedList, n => n.userName == 'b');
+			console.assert(b && b.status == false);
+
+			cb();
+		},
+		cb => {
+			let usName = 'b';
+			let so = soList[usName] = createSocket();
+			login(so, usName);
+			setTimeout(cb, 2000);
+
+		},
+		cb => {
+			// b重新登录
+			let b = _.find(watchedList, n => n.userName == 'b');
+			console.assert(b.status == true);
+			cb();
+		},
+		cb => {
+			console.log(JSON.stringify(watchedList, null, 4));
+			cb();
+		}
+	], clear.bind(null, cb));
+
+
+
+});
+
+createWatcher();
+setTimeout(() => {
+
+	async.eachSeries(testList, (te, cb) => te(cb), () => {
+		console.log('test complete');
+	});
+
+}, 2000);
 
 
 
